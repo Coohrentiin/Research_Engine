18ab19f217d62705229fd346008aa112e69f4ab1
soubeiran <corentin.soubeiran@ensta-paristech.fr>
2019-03-26 22:17:32 +0100

    test des cas d'erreurs, le code ne gère pas les accents dans les mots pour les convertir !

--- TDD/clement/test_bde.py
+++ TDD/clement/test_bde.py
@@ -7,7 +7,7 @@ class Knownvalues(unittest.TestCase):
                          ("Haro","Pierre","pierre.haro@ensta-paristech.fr"),
                          ("De Kocker","Camille","camille.dekocker@ensta-paristech.fr"),
                          ("De Kocker","Camille","camille.de-kocker@ensta-paristech.fr"))
-    known_values_whois=( ("Pioch","Romain","SexGé"),
+    known_values_whois=( ("Pioch","Romain","Secrétaire général"),
                          ("Pioch","Romain","Secretaire General"),
                          ("Soubeiran","corentin","Trésorier"))
     known_values_respo=( ("Présidante","Legrand"),
@@ -24,9 +24,14 @@ class Knownvalues(unittest.TestCase):
             self.assertEqual(statue,result)
 
     def test_respo(self):
-        for statue,firstname in self.known_values_respo:
-            result=bde.findRespo(statue)
+        for statut,firstname in self.known_values_respo:
+            result=bde.findRespo(statut)
             self.assertEqual(firstname,result)
+
+class ToBdeBadInput(unittest.TestCase):
+    def testBadType(self):
+        for s in (("sdfze",0),(56,"zdfze"),(True,"sdg"),("sdfg0",False)):
+            self.assertRaises(bde.NotStringError, bde.toENSTAEmail, s[0], s[1])
 
 if __name__ == '__main__':
     unittest.main()
