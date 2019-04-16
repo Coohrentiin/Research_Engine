ad7d414d398874de6c5939988a0d1e3ea31a598c
Clément CAPORAL <clement.caporal@ensta-paristech.fr>
2019-04-16 18:08:22 +0200

    start user interface

--- catchme/src/index.py
+++ catchme/src/index.py
@@ -10,16 +10,18 @@ class index:
 
     def __init__(self):
         # initialize the index
-        a = open("output/index.json", "w")
+        a = open(os.getcwd()[:-3] + "/output/index.json", "w")
+        a.write("test")
         a.close()
 
-        self.path = os.getcwd() + "/output/" # save where the index is saved
+        self.path = os.getcwd()[:-3] + "output/" # save where the index is saved
         self.index = {} # table with all the words and their TFIDF and in which document they appear
         # for example index = {"hello": [[0, 0.5], [1, 0.2]]}
         self.size = 0 # number of words in the index
         self.corpus = [] # list with the path to the text and its euclidien norm 
         # for example corpus = [["text.txt", 65], ["test3", 0.34]]
         self.size_corpus=0 # number of documents in the corpus
+        print(self.path)
 
     def load_files(self, path):
         """
@@ -33,7 +35,7 @@ class index:
         # start to load each document in the index
         i = 0
         while i < self.size_corpus: # for each document
-            self.add_file(self.corpus[i][0], i) # add the document to the corpus
+            self.add_file_to_index(self.corpus[i][0], i) # add the document to the corpus
             i = i + 1
             if i / 100 % 100 == 0:
                 print(str(int(float(i)/self.size_corpus))+"%")
@@ -94,7 +96,6 @@ class index:
             i = 0
             while i < len(self.index[word]):
                 self.index[word][i][1] = self.TFIDF(word, self.index[word][i][1])
-                #self.EuclidianWeight+=self.index[word][i][1]
                 self.corpus[self.index[word][i][0]][1] += self.index[word][i][1] * self.index[word][i][1]
                 i += 1
         for document in self.corpus:
--- catchme/src/test_index.py
+++ catchme/src/test_index.py
@@ -7,10 +7,11 @@ import index
 
 def test_index_creation():
     ind = index.index()
-    ind.load_files("input/*")
+    ind.load_files(os.getcwd()[:-3] + "input/*")
+    print(os.getcwd()[:-3] + "input/*")
     print(str(ind.size) + " words have been loaded from a corpus of " + str(len(ind.corpus)) + " files")
 
 def test_index_loader():
     ind = index.index()
     ind.load_index("output/index.json", "../output/correspondances.json")
-test_index_loader()
+test_index_creation()
